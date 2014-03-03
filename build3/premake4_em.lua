33e7e3666716fd8f7b408152fffdff37a2a2fe9a
Anthony Liot <anthony.liot@gmail.com>
2014-03-03 22:02:25 +0100

    Remove this file

--- build3/premake4_em.lua
+++
@@ -1,8 +0,0 @@
---  premake4_em.lua
---
---  Created by Anthony Liot.
---  Copyright (c) 2013 Anthony Liot. All rights reserved.
---
---  terminal >> ./premake4_osx --file=premake4_em.lua gmake
-
-print("Emscripten premake4")
