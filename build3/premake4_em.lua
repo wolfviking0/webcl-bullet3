c503107b2a9193948cdc3b073756160e176d87e7
Anthony Liot <anthony.liot@gmail.com>
2014-03-03 13:03:40 +0100

    Build emscripten using premake

---
+++ build3/premake4_em.lua
@@ -0,0 +1,8 @@
+--  premake4_em.lua
+--
+--  Created by Anthony Liot.
+--  Copyright (c) 2013 Anthony Liot. All rights reserved.
+--
+--  terminal >> ./premake4_osx --file=premake4_em.lua gmake
+
+print("Emscripten premake4")
